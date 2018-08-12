03d535bd290f04b91e71f05a9c29937818c1fad5
Christoph Stach <christoph.stach@gmail.com>
2018-08-12 14:35:35 +0200

    added all columns to the dataframe

--- christoph.py
+++ christoph.py
@@ -1,19 +1,53 @@
 import pandas as pd
 
-from typing import Dict
-
-from tensorflow.keras.layers import Dense, Activation
+from tensorflow.keras.layers import Activation
 from tensorflow.keras.models import Sequential
 
 
+def prepare_data_frame(path: str) -> pd.DataFrame:
+    df: pd.DataFrame = pd.read_csv(path)
+
+    df = df[[
+        'MSSubClass', 'MSZoning', 'LotFrontage', 'LotArea', 'Street', 'Alley', 'LotShape', 'LandContour',
+        'Utilities', 'LotConfig', 'LandSlope', 'Neighborhood', 'Condition1', 'Condition2', 'BldgType',
+        'HouseStyle', 'OverallQual', 'OverallCond', 'YearBuilt', 'YearRemodAdd', 'RoofMatl', 'Exterior1st',
+        'Exterior2nd', 'MasVnrType', 'MasVnrArea', 'ExterQual', 'ExterCond', 'Foundation', 'BsmtQual',
+        'BsmtCond', 'BsmtExposure', 'BsmtFinType1', 'BsmtFinSF1', 'BsmtFinType2', 'BsmtFinSF2', 'BsmtUnfSF',
+        'TotalBsmtSF', 'Heating', 'HeatingQC', 'CentralAir', 'Electrical', '1stFlrSF', '2ndFlrSF',
+        'LowQualFinSF', 'GrLivArea', 'BsmtFullBath', 'BsmtHalfBath', 'FullBath', 'HalfBath', 'TotRmsAbvGrd',
+        'Functional', 'Fireplaces', 'FireplaceQu', 'GarageType', 'GarageYrBlt',
+        'GarageFinish', 'GarageCars', 'GarageArea', 'GarageQual', 'GarageCond', 'PavedDrive', 'WoodDeckSF',
+        'OpenPorchSF', 'EnclosedPorch', '3SsnPorch', 'ScreenPorch', 'PoolArea', 'PoolQC', 'Fence', 'MiscFeature',
+        'MiscVal', 'MoSold', 'YrSold', 'SaleType', 'SaleCondition'
+    ]]
+
+    categorical_columns = [
+        'MSZoning', 'Street', 'Alley', 'LotShape', 'LandContour', 'Utilities', 'LotConfig', 'LandSlope',
+        'Neighborhood', 'Condition1', 'Condition2', 'BldgType', 'HouseStyle', 'RoofMatl', 'Exterior1st', 'Exterior2nd',
+        'MasVnrType', 'ExterQual', 'ExterCond', 'BsmtQual', 'BsmtCond', 'BsmtExposure', 'BsmtFinType1', 'BsmtFinType2',
+        'Heating', 'HeatingQC', 'CentralAir', 'Electrical', 'Functional', 'FireplaceQu', 'GarageType', 'GarageFinish',
+        'GarageQual', 'GarageCond', 'PavedDrive', 'WoodDeckSF', 'PoolQC', 'Fence', 'MiscVal', 'SaleType',
+        'SaleCondition'
+    ]
+
+    df.fillna(-1)
+
+    for column in categorical_columns:
+        df[column] = df[column].astype('category')
+        df[column] = df[column].cat.codes
+
+    
+
+    return df
+
+
 def get_train_test():
-    df_train: pd.DataFrame = pd.read_csv('data/train.csv')
-    df_test: pd.DataFrame = pd.read_csv('data/test.csv')
+    df_train = prepare_data_frame('data/train.csv')
 
-    df_train = df_train[['LotFrontage', 'LotArea']]
+    print(df_train.head())
 
+    print(df_train.count())
 
-    print(df_train.head(10))
     pass
 
 
